05c0e22b8232b279fb90587e6b2f6a350a2339e6
gaurav parida <funkygp@gmail.com>
2013-07-10 00:23:27 +0530

    read file

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+var infile = "index.html";
 
-var app = express.createServer(express.logger());
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.send(new Buffer(fs.readFileSync('index.html', 'utf-8')).toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
